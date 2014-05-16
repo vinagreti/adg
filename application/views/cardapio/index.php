ec44c9dc3ed40d47686250ba6dcb060467c0ca43
vinagreti <bruno@tzadi.com>
2014-05-15 22:14:50 -0300

    cardapio - imagens quadradas

--- application/views/cardapio/index.php
+++ application/views/cardapio/index.php
@@ -3,21 +3,21 @@
 
         <h2>Sanduiches</h2>
 
-        <a href="sanduiches"><img class="img-rounded img-button" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRb0cyNwJf-UVvXQbQoTt7ax-3nARhMyUefsV0DX_fpeLvKbGgwhw"></a>
+        <a href="sanduiches"><img class="img-button" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRb0cyNwJf-UVvXQbQoTt7ax-3nARhMyUefsV0DX_fpeLvKbGgwhw"></a>
 
     </div>
     <div class="col-md-4">
 
         <h2>Pastéis</h2>
 
-        <a href="pasteis"><img class="img-rounded img-button" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSfsUu5bkwGL2sTaNk3TzN6DtfwZeeX_hIHmLpr8WlMZ4zQYSMW"></a>
+        <a href="pasteis"><img class="img-button" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSfsUu5bkwGL2sTaNk3TzN6DtfwZeeX_hIHmLpr8WlMZ4zQYSMW"></a>
 
     </div>
     <div class="col-md-4">
 
         <h2>Bolos</h2>
 
-        <a href="bolos"><img class="img-rounded img-button" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSfNNu1T6LNnemsngPEZSuq2yhzKD7RXXfOtgbKNggs8QB1sRB2"></a>
+        <a href="bolos"><img class="img-button" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSfNNu1T6LNnemsngPEZSuq2yhzKD7RXXfOtgbKNggs8QB1sRB2"></a>
 
     </div>
 </div>
@@ -26,21 +26,21 @@
 
         <h2>Geléias</h2>
 
-        <a href="geleias"><img class="img-rounded img-button" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeWUyu4m0Gew_LFaWQsYjiJ8yVi04WhaKGNFoMOBSNMUmHHk0Ivg"></a>
+        <a href="geleias"><img class="img-button" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeWUyu4m0Gew_LFaWQsYjiJ8yVi04WhaKGNFoMOBSNMUmHHk0Ivg"></a>
 
     </div>
     <div class="col-md-4">
 
         <h2>Licores</h2>
 
-        <a href="licores"><img class="img-rounded img-button" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQAzH8GFqfn5mNZ7dfdGm7zXDPzYLLURiOjh1sRdD-Emn8oHQpi"></a>
+        <a href="licores"><img class="img-button" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQAzH8GFqfn5mNZ7dfdGm7zXDPzYLLURiOjh1sRdD-Emn8oHQpi"></a>
 
     </div>
     <div class="col-md-4">
 
         <h2>Saladas</h2>
 
-        <a href="saladas"><img class="img-rounded img-button" src="http://www.dicasdemulher.com.br/wp-content/uploads/2011/11/3-saladas-verao.jpg"></a>
+        <a href="saladas"><img class="img-button" src="http://www.dicasdemulher.com.br/wp-content/uploads/2011/11/3-saladas-verao.jpg"></a>
 
     </div>
 
