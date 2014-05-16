e2b1df9b068130398ed3d96d79c64f8628cfc395
vinagreti <bruno@tzadi.com>
2014-05-15 22:11:22 -0300

    titulo nos icones do cardapio

--- application/views/cardapio/index.php
+++ application/views/cardapio/index.php
@@ -1,46 +1,46 @@
 <div class="row">
     <div class="col-md-4">
 
-        <a href="sanduiches"><img class="img-rounded img-button" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRb0cyNwJf-UVvXQbQoTt7ax-3nARhMyUefsV0DX_fpeLvKbGgwhw"></a>
+        <h2>Sanduiches</h2>
 
-        Sanduiches
+        <a href="sanduiches"><img class="img-rounded img-button" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRb0cyNwJf-UVvXQbQoTt7ax-3nARhMyUefsV0DX_fpeLvKbGgwhw"></a>
 
     </div>
     <div class="col-md-4">
+
+        <h2>Pastéis</h2>
 
         <a href="pasteis"><img class="img-rounded img-button" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSfsUu5bkwGL2sTaNk3TzN6DtfwZeeX_hIHmLpr8WlMZ4zQYSMW"></a>
 
-        Pastéis
-
     </div>
     <div class="col-md-4">
 
-        <a href="bolos"><img class="img-rounded img-button" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSfNNu1T6LNnemsngPEZSuq2yhzKD7RXXfOtgbKNggs8QB1sRB2"></a>
+        <h2>Bolos</h2>
 
-        Bolos
+        <a href="bolos"><img class="img-rounded img-button" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSfNNu1T6LNnemsngPEZSuq2yhzKD7RXXfOtgbKNggs8QB1sRB2"></a>
 
     </div>
 </div>
 <div class="row">
     <div class="col-md-4">
 
-        <a href="geleias"><img class="img-rounded img-button" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeWUyu4m0Gew_LFaWQsYjiJ8yVi04WhaKGNFoMOBSNMUmHHk0Ivg"></a>
+        <h2>Geléias</h2>
 
-        Geléias
+        <a href="geleias"><img class="img-rounded img-button" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeWUyu4m0Gew_LFaWQsYjiJ8yVi04WhaKGNFoMOBSNMUmHHk0Ivg"></a>
 
     </div>
     <div class="col-md-4">
+
+        <h2>Licores</h2>
 
         <a href="licores"><img class="img-rounded img-button" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQAzH8GFqfn5mNZ7dfdGm7zXDPzYLLURiOjh1sRdD-Emn8oHQpi"></a>
 
-        Licores
-
     </div>
     <div class="col-md-4">
 
-        <a href="saladas"><img class="img-rounded img-button" src="http://www.dicasdemulher.com.br/wp-content/uploads/2011/11/3-saladas-verao.jpg"></a>
+        <h2>Saladas</h2>
 
-        Saladas
+        <a href="saladas"><img class="img-rounded img-button" src="http://www.dicasdemulher.com.br/wp-content/uploads/2011/11/3-saladas-verao.jpg"></a>
 
     </div>
 
